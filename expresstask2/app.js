945954e810de6ef47abd960bec9e915850de4543
Shreyaskr0 <shreyaskr2001@gmail.com>
2025-02-16 16:02:17 +0530

    expresstask2

---
+++ expresstask2/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser =require('body-parser');
+const { truncateSync } = require('fs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<html><body><form action="/product" method="POST">' +
+             '<input type="text" name="title" placeholder="Product Name">' +
+             '<br><br>' +
+             '<input type="text" name="size" placeholder="Product Size">' +
+             '<br><br>' +
+             '<button type="submit">Add Product</button>' +
+             '</form></body></html>');
+});
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+    });
+
+app.use('/',(req, res, next) => {
+res. send('<h1>Hello from Express!</h1>');
+});
+
+app. listen (3000);
