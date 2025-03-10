90d3b8f66a75ffb7d960da2afe9edf594934c0a0
Shreyaskr0 <shreyaskr2001@gmail.com>
2025-03-11 01:19:18 +0530

    ecom controllers

---
+++ expresstask17/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const router=express.Router();
+const productController=require('../controllers/productController')
+
+router.get("/",productController.getProducts);
+
+router.post("/",productController.postProducts);
+
+router.get("/:id",productController.getProductsByID);
+
+router.put("/:id",productController.editProducts);
+
+router.delete("/:id",productController.deleteProducts);
+
+module.exports = router;
+
